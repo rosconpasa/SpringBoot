601b2c622781e7c33f287a12b3fc1f824c503007
rosconpasa <willdozawill@gmail.com>
2025-12-16 18:32:17 +0000

    problema github

--- demo/src/main/java/com/prueba/demo/controlador/ProductoController.java
+++ demo/src/main/java/com/prueba/demo/controlador/ProductoController.java
@@ -1,17 +1,7 @@
 package com.prueba.demo.controlador;
 
-
-
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.prueba.demo.servicio.ProductoService;
 import com.prueba.demo.modelo.Producto;
